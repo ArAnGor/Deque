fdc13fa850313fefeb31836e3ba8b663ef24c0dd
ArAnGor <gorbart01@mail.ru>
2021-03-15 17:43:31 +0300

    try to fix travis 4

--- include/Deque.h
+++ include/Deque.h
@@ -1,26 +1,26 @@
 #pragma once
 
-template <class T>
-class Deque
-{
-private:
-
-  template <class T>
-  class ListElem
-  {
-  public:
-    ListElem<T> *prev;
-    T *data;
-    ListElem<T> *next;
-
-    ListElem(ListElem<T> &p, T &d, ListElem<T> &n);
-    ~ListElem();
-  };
-
-  ListElem<T> head;
-  ListElem<T> tail;
-
-public:
-  Deque();
-  ~Deque();
-};
+//template <class T>
+//class Deque
+//{
+//private:
+//
+//  template <class T>
+//  class ListElem
+//  {
+//  public:
+//    ListElem<T> *prev;
+//    T *data;
+//    ListElem<T> *next;
+//
+//    ListElem(ListElem<T> &p, T &d, ListElem<T> &n);
+//    ~ListElem();
+//  };
+//
+//  ListElem<T> head;
+//  ListElem<T> tail;
+//
+//public:
+//  Deque();
+//  ~Deque();
+//};
