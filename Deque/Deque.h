994931ce214ff0a945b47c95d87decd45e373daf
ArAnGor <gorbart01@mail.ru>
2021-04-06 00:00:26 +0300

    add methods

--- Deque/Deque.h
+++ Deque/Deque.h
@@ -12,11 +12,11 @@ private:
   {
   public:
     ListElem<T> *prev;
-    T *data;
+    T data;
     ListElem<T> *next;
 
-    ListElem(ListElem<T> *p, T *d, ListElem<T> *n) { prev = p; data = d; next = n; }
-    ~ListElem() { delete data; }
+    ListElem(ListElem<T> *p, T &d, ListElem<T> *n) { prev = p; data = d; next = n; }
+    ~ListElem() {}
   };
 
   ListElem<T> *head;
@@ -37,53 +37,80 @@ public:
 template <class T>
 Deque<T>::Deque()
 {
-  head = new ListElem<T>(NULL, NULL, NULL);
-  tail = new ListElem<T>(head, NULL, NULL);
+  T t;
+  head = new ListElem<T>(NULL, t, NULL);
+  tail = new ListElem<T>(head, t, NULL);
   head->next = tail;
 }
 
 template <class T>
 Deque<T>::~Deque()
 {
-
+  while (!IsEmpty())
+  {
+    tail->prev = tail->prev->prev;
+    delete tail->prev->next;
+    tail->prev->next = tail;
+  }
+  delete head;
+  delete tail;
 }
 
 template<class T>
 T Deque<T>::GetFront() const
 {
-  return T();
+  if (IsEmpty()) throw exception();
+  return head->next->data;
 }
 
 template<class T>
 T Deque<T>::GetBack() const
 {
-  return T();
+  if (IsEmpty()) throw exception();
+  return tail->prev->data;
 }
 
 template<class T>
 T Deque<T>::PopFront()
 {
-  return T();
+  if (IsEmpty()) throw exception();
+  T data = head->next->data;
+  head->next = head->next->next;
+  delete head->next->prev;
+  head->next->prev = head;
+  return data;
 }
 
 template<class T>
 T Deque<T>::PopBack()
 {
-  return T();
+  if (IsEmpty()) throw exception();
+  T data = tail->prev->data;
+  tail->prev = tail->prev->prev;
+  delete tail->prev->next;
+  tail->prev->next = tail;
+  return data;
 }
 
 template<class T>
 void Deque<T>::PushFront(T &elem)
 {
+  ListElem<T> *le = head->next;
+  head->next = new ListElem<T>(head, elem, le);
+  le->prev = head->next;
 }
 
 template<class T>
 void Deque<T>::PushBack(T &elem)
 {
+  ListElem<T> *le = tail->prev;
+  tail->prev = new ListElem<T>(le, elem, tail);
+  le->next = tail->prev;
 }
 
 template<class T>
 bool Deque<T>::IsEmpty() const
 {
+  if (head->next == tail) return true;
   return false;
 }
